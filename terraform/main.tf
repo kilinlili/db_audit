6cc35db02c44d280e5d41e12cc7fdacefa4c04f9
kilinlili <rin.ueno@cpc.ait.kyushu-u.ac.jp>
2023-12-08 03:15:40 +0900

    minor fixed

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,74 +8,75 @@ provider "aws" {
 }
 
 resource "aws_db_instance" "postgresql_instance" {
-  identifier        = "sampledb"
-  allocated_storage = 20
-  storage_type      = "gp2"
-  engine            = "postgres"
-  engine_version    = "12"
-  instance_class    = "db.t2.micro"
+  identifier           = "sampledb"
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "postgres"
+  engine_version       = "12"
+  instance_class       = "db.t2.micro"
   username             = "postgres"
   password             = "postgres"
   parameter_group_name = "default.postgres12"
+  skip_final_snapshot  = true
 }
 
-resource "aws_lambda_function" "alert_message_lambda" {
-  filename      = "../function.zip"
-  function_name = "alert_email"
-  role          = aws_iam_role.lambda_exec.arn
-  handler       = "index"
-  runtime       = "nodejs18.x"
-}
+# resource "aws_lambda_function" "alert_message_lambda" {
+#   filename      = "../function.zip"
+#   function_name = "alert_email"
+#   role          = aws_iam_role.lambda_exec.arn
+#   handler       = "index"
+#   runtime       = "nodejs18.x"
+# }
 
-resource "aws_iam_role" "lambda_exec" {
-  # IAM role for Lambda execution
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = "sts:AssumeRole",
-        Effect = "Allow",
-        Principal = {
-          Service = "lambda.amazonaws.com",
-        },
-      },
-    ],
-  })
-}
+# resource "aws_iam_role" "lambda_exec" {
+#   # IAM role for Lambda execution
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [
+#       {
+#         Action = "sts:AssumeRole",
+#         Effect = "Allow",
+#         Principal = {
+#           Service = "lambda.amazonaws.com",
+#         },
+#       },
+#     ],
+#   })
+# }
 
-# cloudwatch logs
-resource "aws_cloudwatch_log_group" "postgresql_log_group" {
-  name              = "/aws/rds/instance/${aws_db_instance.postgresql_instance.identifier}/postgresql"
-  retention_in_days = 7
-}
+# # cloudwatch logs
+# resource "aws_cloudwatch_log_group" "postgresql_log_group" {
+#   name              = "/aws/rds/instance/${aws_db_instance.postgresql_instance.identifier}/postgresql"
+#   retention_in_days = 7
+# }
 
-# subscriptionfilter
-resource "aws_cloudwatch_log_subscription_filter" "lambda_subscription_filter" {
-  name            = "query"
-  log_group_name  = aws_cloudwatch_log_group.postgresql_log_group.name
-  filter_pattern  = "OBJECT"
-  destination_arn = aws_lambda_function.alert_message_lambda.arn
-}
-
-
-# cloudWatch logs => lambda
-resource "aws_lambda_permission" "allow_cloudwatch_logs" {
-  statement_id  = "AllowExecutionFromCloudWatchLogs"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.alert_message_lambda.function_name
-  principal     = "logs.amazonaws.com"
-  source_arn    = aws_cloudwatch_log_group.postgresql_log_group.arn
-}
+# # subscriptionfilter
+# resource "aws_cloudwatch_log_subscription_filter" "lambda_subscription_filter" {
+#   name            = "query"
+#   log_group_name  = aws_cloudwatch_log_group.postgresql_log_group.name
+#   filter_pattern  = "OBJECT"
+#   destination_arn = aws_lambda_function.alert_message_lambda.arn
+# }
 
 
-# SNS topic
-resource "aws_sns_topic" "email_topic" {
-  name = "your_sns_topic_name"
-}
+# # cloudWatch logs => lambda
+# resource "aws_lambda_permission" "allow_cloudwatch_logs" {
+#   statement_id  = "AllowExecutionFromCloudWatchLogs"
+#   action        = "lambda:InvokeFunction"
+#   function_name = aws_lambda_function.alert_message_lambda.function_name
+#   principal     = "logs.amazonaws.com"
+#   source_arn    = aws_cloudwatch_log_group.postgresql_log_group.arn
+# }
 
-# SNS subscription
-resource "aws_sns_topic_subscription" "example_subscription" {
-  topic_arn = aws_sns_topic.email_topic.arn
-  protocol  = "email"
-  endpoint  = "your_email@example.com"
-}
+
+# # SNS topic
+# resource "aws_sns_topic" "email_topic" {
+#   name = "your_sns_topic_name"
+# }
+
+# # SNS subscription
+# resource "aws_sns_topic_subscription" "example_subscription" {
+#   topic_arn = aws_sns_topic.email_topic.arn
+#   protocol  = "email"
+#   endpoint  = "your_email@example.com"
+# }
